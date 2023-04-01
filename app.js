cde49b6d87e6d672f754ab12df6efbc56ffa300b
RobertScalfani <robis345@gmail.com>
2023-04-01 12:42:14 -0400

    close to finished with a8

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import cors from 'cors'
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
+// API Controllers.
 HelloController(app);
-
 UserController(app);
+TuitsController(app);
 
 app.listen(4000);
